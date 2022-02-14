744b1a4cf30840c0373a366f882151400512a347
enrico <enricoxpe@hotmail.it>
2022-02-14 13:52:43 +0100

    first commit

---
+++ main.ts
@@ -0,0 +1,19 @@
+console.log('Hello World!');
+
+let myVar: number = 3;
+myVar += 10;
+
+console.log(myVar);
+
+interface MyInterface {
+    myNumber: number;
+    myString: string;
+}
+
+let myInterface: MyInterface = {
+    myNumber: 10,
+    myString: 'Hello World',
+};
+
+console.log(myInterface.myNumber);
+console.log(myInterface.myString);
